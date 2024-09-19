01d28ac4799f723f494fa4e8b83bb38d16212fb4
Julian-Banks <bnkjul001@myuct.ac.za>
2024-09-19 08:05:23 +0700

    Update display.py

--- moths_lighting/display.py
+++ moths_lighting/display.py
@@ -218,10 +218,10 @@ class Display:
                 
                 colour_submenu = Menu(colour_name, items=[
                     
-                    AdjustableMenuItem("Red", self.get_red, self.set_red, min_value=0, max_value=255, step=1),
-                    AdjustableMenuItem("Green", self.get_green, self.set_green, min_value=0, max_value=255, step=1),
-                    AdjustableMenuItem("Blue", self.get_blue, self.set_blue, min_value=0, max_value=255, step=1),
-                    AdjustableMenuItem("Display Colour", self.get_display_colour, self.set_display_colour, min_value=0, max_value=1, step=1),
+                    AdjustableMenuItem("Red", get_red, set_red, min_value=0, max_value=255, step=1),
+                    AdjustableMenuItem("Green", get_green, set_green, min_value=0, max_value=255, step=1),
+                    AdjustableMenuItem("Blue", get_blue, set_blue, min_value=0, max_value=255, step=1),
+                    AdjustableMenuItem("Display Colour", get_display_colour, set_display_colour, min_value=0, max_value=1, step=1),
                     MenuItem("Remove Colour", action= self.colour_manager.remove_colour(idx)),
                     MenuItem("Back", action = update_colour)
                 
