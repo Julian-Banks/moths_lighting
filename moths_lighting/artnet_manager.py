c97a97531f8472b257300006cef781749c69b298
Julian-Banks <bnkjul001@myuct.ac.za>
2024-09-23 21:38:56 +0700

    working on artnet manger

--- moths_lighting/artnet_manager.py
+++ moths_lighting/artnet_manager.py
@@ -27,7 +27,7 @@ class ArtnetManager:
             return
         
         for universe, artnet_instance in enumerate(self.artnet_instances):
-            start = universe * self.pixels_per_universe + 1
+            start = universe * self.pixels_per_universe if universe == 0 else universe * self.pixels_per_universe + 1
             end = (universe + 1) * self.pixels_per_universe if universe < self.num_universes - 1 else len(data)
             
             print(f'Sending {end-start} pixels to universe {universe}')
