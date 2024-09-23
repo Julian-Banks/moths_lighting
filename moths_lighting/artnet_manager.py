e76b1cbad6d79e8fde641dcae679f6aa01d5a0f7
Julian-Banks <bnkjul001@myuct.ac.za>
2024-09-23 21:20:53 +0700

    Update artnet_manager.py

--- moths_lighting/artnet_manager.py
+++ moths_lighting/artnet_manager.py
@@ -12,14 +12,15 @@ class ArtnetManager:
         
         self.pixels_per_universe = 512
         
-        num_universes = self.packet_size // self.pixels_per_universe + 1
+        self.num_universes = self.packet_size // self.pixels_per_universe + 1
         
-        for universe in range(num_universes):
-            packet_size = self.pixels_per_universe if universe < num_universes - 1 else self.packet_size % self.pixels_per_universe
+        for universe in range(self.num_universes):
+            packet_size = self.pixels_per_universe if universe < self.num_universes - 1 else self.packet_size % self.pixels_per_universe
             self.artnet_instances.append(StupidArtnet(self.target_ip, universe, packet_size ,self.fps,True, True))
         
     def send(self, data):
-        for i, artnet_instance in enumerate(self.artnet_instances):
-            start = i * 1
-            end = (i + 1) * self.pixels_per_universe
+        for universe, artnet_instance in enumerate(self.artnet_instances):
+            start = universe * 1
+            end = (universe + 1) * self.pixels_per_universe if universe < self.num_universes - 1 else len(data)
+            
             artnet_instance.send(data[start:end])
