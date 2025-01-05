6ec54a6c7cd24328ccb47c78d8edc764effabcee
Julian-Banks <julianrowlandbanks@gmail.com>
2025-01-05 11:50:45 +0200

    Update artnet.py

--- moths_lighting/artnet.py
+++ moths_lighting/artnet.py
@@ -295,7 +295,7 @@ class ArtnetController:
                     bar = self.device_bars_map[artnet_device][0]
                     bar.colour_manager.add_colour(colour)
                     bar.colour_manager.update_config()
-                    bar.update_colours()
+        self.update_bar_colours()
                     
                         
     def update_colour(self, idx, colour):
@@ -304,17 +304,17 @@ class ArtnetController:
                 if self.device_bars_map[artnet_device]:
                     bar = self.device_bars_map[artnet_device][0]
                     bar.colour_manager.update_colour(idx, colour)
-                    bar.update_colours()
                     bar.colour_manager.update_config()
-                        
+        self.update_bar_colours()
+        
     def remove_colour(self, idx ):
         for artnet_device in self.artnet_devices: 
             if artnet_device.edit_config :
                 if self.device_bars_map[artnet_device]:
                     bar = self.device_bars_map[artnet_device][0]
                     bar.colour_manager.remove_colour(idx )
-                    bar.update_colours()
                     bar.colour_manager.update_config()
+        self.update_bar_colours()
                         
     def get_colour_list(self):
         for artnet_device in self.artnet_devices: 
